e121e55ac5216db968d8088e553f6386a3be1714
zouhao <zouhao@tp-link.com.cn>
2020-12-29 11:40:12 +0800

    Initial Commit

---
+++ Shared/yolov3DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  yolov3DemoApp.swift
+//  Shared
+//
+//  Created by zouhao on 2020/12/29.
+//
+
+import SwiftUI
+
+@main
+struct yolov3DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
